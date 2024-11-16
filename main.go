da38a4989ebc8a18a41126eac8694eb4a36c0a49
Vismay Bhargav <122339161+vismaybhargav@users.noreply.github.com>
2024-11-16 13:45:10 -0800

    (add) added bubbletea integration

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+)
 
 func main() {
+	p := tea.NewProgram()
 	fmt.Println("Hello World")
+}
+
+func InitModel() Model {
+	return Game{}
 }
